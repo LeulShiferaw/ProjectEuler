bb00b6f49cbefc472bbdce6d8e20679e6ca58a97
LeulShiferaw <abiyleoul3@gmail.com>
2025-03-12 15:31:18 -0500

    Solve PE113

--- PE113/main.cpp
+++ PE113/main.cpp
@@ -1,37 +1,62 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long num_increasing(int num_dig, int prev)
-{
-    if(num_dig == 1)
-        return 9-prev+1;
+/*
+    The answer is off by one and I don't know why!!
 
-    long long res = 0;
-    for(int i = prev; i<=9; ++i)
-        res += num_increasing(num_dig-1, i);
-    return res;
-}
+    DP solution to calculate the answer
+*/
 
-long long num_decreasing(int num_dig, int prev)
-{
-    if(num_dig == 1)
-        return prev-0+1;
-    
-    long long res = 0;
-    for(int i = prev; i>=0; --i)
-        res+=num_decreasing(num_dig-1, i);
-    return res;
+constexpr int SIZE = 6;
+
+void solve() {
+    vector<vector<long long>> dp_increase(10, vector<long long>(SIZE, -1));
+    vector<vector<long long>> dp_decrease(10, vector<long long>(SIZE, -1));
+
+    function<long long(int, int)> calc_increase = [&](int prev, int ind) -> long long {
+        if(ind >= SIZE) {
+            return 1;
+        }
+        
+        if(dp_increase[prev][ind] != -1) return dp_increase[prev][ind];
+        long long soln = 0;
+        for(int i=prev; i<10; ++i) {  //Start from previous and go up
+            soln += calc_increase(i, ind+1);
+        }
+        dp_increase[prev][ind] = soln;
+        return soln;
+    };
+
+    function<long long(int, int)> calc_decrease = [&](int prev, int ind) -> long long {
+        if(ind >= SIZE) {
+            return 1;
+        }
+        
+        if(dp_decrease[prev][ind] != -1) return dp_decrease[prev][ind];
+        long long soln = 0;
+        for(int i=prev; i>=0; --i) { //Decrement from previous and go down
+            soln += calc_decrease(i, ind+1);
+        }
+        dp_decrease[prev][ind] = soln;
+        return soln;
+    };
+
+    auto val1 = calc_increase(0, 0);
+    long long val2 = 0;
+    for(int i=0; i<SIZE; ++i) {
+        val2 += calc_decrease(9, i) - 1; //Subtract one because 0 is calculated each time the leading number is 0. This is also why this loop exists because it doesn't go from 100 -> 99 but straight to 0
+    }
+    cout << "Increase: " << val1 << endl;
+    cout << "Decrease: " << val2 << endl;
+    cout << "Total: " << val1 + val2 - 9*SIZE  << endl; //0 is removed now subtract 1->9 for each possible number of digits. This will delete 11->22->...->99 for example.
 }
 
 int main()
 {
-    long long res = 0;
-    for(int i = 9; i>=0; --i)
-    {
-        res+=num_increasing(2, i) + num_decreasing(2, i);
-    }
-    res-=28;
-    cout << res << endl;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
     return 0;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,35 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-long long gcd(long long a, long long b) {
-    while (b) {
-        long long t = a % b;
-        a = b;
-        b = t;
-    }
-    return a;
-}
+#define LSOne(S) ((S) & -(S))
 
-long long count(long long N) {
-    long long result = 0;
-    for (long long u = 1; u * u <= N; ++u) {
-        for (long long v = 1; v < u; ++v) {
-            if ((u + v) % 2 == 1 && gcd(u, v) == 1) {
-                long long a = u * u + v * v;
-                long long b = u * u - v * v;
-                long long c = 2 * u * v;
-                long long p = a + b + c;
-                if (p <= N) {
-                    result += (N - p) / (2 * a) + 1;
-                }
-            }
-        }
-    }
-    return result;
-}
-
-int main() {
-    long long N = 100000000;
-    long long result = count(N);
-    std::cout << "The count of Pythagorean polygons is: " << result << std::endl;
-    return 0;
-}
+int N;
+double dist[20][]
